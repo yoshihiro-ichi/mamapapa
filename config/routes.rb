2a5a993349867cb901976336ec6c46878f673d76
yoiyo <aden.yi21120821@gmail.com>
2021-08-12 11:50:17 +0900

    ゲストログイン作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,15 @@ Rails.application.routes.draw do
   root 'facilities#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  # resources :facilities do
-  #   member do
-  #     patch:confirm
-  #   end
-  # end
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.nema = "ゲスト"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
