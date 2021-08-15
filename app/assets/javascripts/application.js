0a4b7ce0892c4eff1b59418191d5a5c364d21f7d
yoiyo <aden.yi21120821@gmail.com>
2021-08-16 05:14:36 +0900

    herokuエラーのため修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //= require rails-ujs
 //= require activestorage
-//= require jquery/dist/jquery.js
+//= require jquery
 //= require bootstrap/dist/js/bootstrap.min
 //= require turbolinks
 //= require_tree .
