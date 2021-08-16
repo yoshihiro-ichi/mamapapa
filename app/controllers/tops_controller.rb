f3acb3e4a29dc37a2045e4760f92bd3e5a8aa259
yoiyo <aden.yi21120821@gmail.com>
2021-08-16 15:04:53 +0900

    top画面のひな形作成#39

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,2 +1,4 @@
 class TopsController < ApplicationController
+def index
+ @tops = Top.all
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
     end
   end
   resources :favorites, only: [:create, :destroy,:index]
-
-  root 'facilities#index'
+  # get  'top#index'
+  root 'top#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
