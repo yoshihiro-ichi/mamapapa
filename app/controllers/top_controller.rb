65b2d5c05f8dcbbcef6931044bd7df81a4a1aa53
yoiyo <aden.yi21120821@gmail.com>
2021-08-16 17:49:43 +0900

    フッター調整未完成＃45

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,5 @@
 class TopController < ApplicationController
   before_action :authenticate_user!, except: %i[index]
-
   def index
      if user_signed_in?
        redirect_to facilities_path(current_user)
