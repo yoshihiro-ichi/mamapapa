330ff36fc2e340ccdf76f2d4c8c368c12671ef97
yoiyo <aden.yi21120821@gmail.com>
2021-08-21 12:11:37 +0900

    add:施設が降順に表示されるテストを実装#50

--- spec/system/facilities_spec.rb
+++ spec/system/facilities_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 RSpec.describe '施設投稿機能', type: :system do
   let!(:user){ FactoryBot.create(:user,email: 'yoshi@a.com') }
+  let!(:user){ FactoryBot.create(:user2,email: 'yoshi2@a.com') }
+  let!(:user){ FactoryBot.create(:user3,email: 'yoshi3@a.com') }
   let!(:facility) { FactoryBot.create(:facility,user_id: user.id)}
-  let!(:facility){ FactoryBot.create(:facility)}
-  # let!(:facility){create(:facility2)}
-  # let!(:facility){create(:facility3,)}
+  let!(:facility2){FactoryBot.create(:facility2,user_id:user.id)}
+  let!(:facility3){FactoryBot.create(:facility3,user_id:user.id)}
+
   before do
     visit new_user_session_path
     fill_in :user_email, with: "yoshi@a.com"
+    fill_in :user_password, with: "123456"
+    click_on 'commit'
+    visit new_user_session_path
+    fill_in :user_email, with: "yoshi2@a.com"
+    fill_in :user_password, with: "123456"
+    click_on 'commit'
+    visit new_user_session_path
+    fill_in :user_email, with: "yoshi3@a.com"
     fill_in :user_password, with: "123456"
     click_on 'commit'
   end
@@ -36,12 +46,18 @@ RSpec.describe '施設投稿機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に移行した場合' do
       it '作成済みのタスク一覧が表示される' do
-        ecpect(page).to have_content '喫茶ロード'
+        visit facilities_path
+          expect(page).to have_content '喫茶ロード'
+      end
+    end
+    context '施設投稿作成日時の降順に並んでいる場合'do
+      it '新しいタスクが一番上に表示される' do
+        facility_list = all('.facility_low')
+          expect(facility_list[0]).to have_content 'タイトル3'
+
       end
     end
   end
-
-
 
 
 
