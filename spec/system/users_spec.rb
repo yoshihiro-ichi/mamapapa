08d626173370f8768553f1301b2dde809292853d
yoiyo <aden.yi21120821@gmail.com>
2021-08-22 04:21:24 +0900

    add:user関連の新規登録とログイン機能のテスト#50

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+RSpec.describe 'User関連機能', type: :system do
+  let!(:user){ FactoryBot.create(:user,email: 'yoshi@a.com') }
+  before do
+    visit root_path
+  end
+  describe '新規登録機能' do
+   context '全ての記述が正しい場合' do
+      it '新規登録できる' do
+       click_on "新規登録"
+       fill_in "user[name]", with: "いち"
+       fill_in "user[email]", with: "a@a.com"
+       fill_in "user[password]", with: "123456"
+       fill_in "user[password_confirmation]", with: "123456"
+       click_button 'commit'
+       expect(page).to have_content '施設投稿一覧'
+       expect(page).to have_content 'アカウント登録が完了しました。'
+      end
+    end
+    context '入力欄が空だった場合' do
+      it '新規登録出来ずエラー文が出る' do
+        click_on "新規登録"
+        click_on "commit"
+        expect(page).to have_content'5 件のエラーが発生したため ユーザー は保存されませんでした。'
+      end
+    end
+  end
+  describe 'ログイン機能' do
+    context '全ての記述が正しい場合' do
+       it 'ログインできる' do
+         click_on "ログイン"
+         fill_in "user[email]",with: "yoshi@a.com"
+         fill_in "user[password]", with: "123456"
+         click_on "commit"
+         expect(page).to have_content 'ログインしました'
+      end
+    end
+    context 'emailとpasswordが一致しない場合' do
+       it 'ログインできない' do
+         click_on "ログイン"
+        fill_in "user[email]",with: "yoshi@a.com"
+        fill_in "user[password]", with: "987654"
+        click_on "commit"
+        expect(page).to have_content 'メールアドレスまたはパスワードが違います。'
+      end
+    end
+  end
+end
