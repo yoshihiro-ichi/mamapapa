9d11ee48b8a24c8510ca246a94e52b49789c0804
yoiyo <aden.yi21120821@gmail.com>
2021-08-24 23:22:26 +0900

    facilitiestebl　preficutreのデータ型を変更#77

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_171254) do
+ActiveRecord::Schema.define(version: 2021_08_24_140926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_171254) do
   create_table "facilities", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.string "prefecture"
+    t.integer "prefecture"
     t.string "address"
     t.string "price"
     t.string "types"
