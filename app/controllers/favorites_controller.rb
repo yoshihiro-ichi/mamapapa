0dddce9a3966b9f1f0f9fe142f3bad415a4972dd
yoiyo <aden.yi21120821@gmail.com>
2021-08-26 17:34:45 +0900

    お気に入り登録解除時に一覧画面遷移する仕様を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,10 +6,12 @@ end
 
   def create
     favorite = current_user.favorites.create(facility_id: params[:facility_id])
-    redirect_to facilities_path, notice: "#{favorite.facility.user.name}さんのブログをお気に入り登録しました"
+    favorites = User.find_by(id: favorite.facility_id)
+    redirect_to facility_path(favorites.id), notice: "#{favorite.facility.user.name}さんのブログをお気に入り登録しました"
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to facilities_path, notice: "#{favorite.facility.user.name}さんのブログをお気に入り解除しました"
+    favorites = User.find_by(id: favorite.facility_id)
+    redirect_to facility_path(favorites.id), notice: "#{favorite.facility.user.name}さんのブログをお気に入り解除しました"
   end
 end
