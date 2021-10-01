d95efc57f184f6fc7857a11a9c346b89d32704e6
yoiyo <aden.yi21120821@gmail.com>
2021-10-01 16:56:43 +0900

    add:slicをtop画面に導入#35

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
+$(function() {
+    $('.contents2').slick({
+      autoplay: true,         //自動再生
+	    autoplaySpeed: 2000,　  //自動再生のスピード
+	    speed: 800,		　 //スライドするスピード
+	    dots: true,　　　　　　//スライドしたのドット
+	    arrows: true,          //左右の矢印
+	    infinite: true,　　　　//スライドのループ
+	    pauseOnHover: false,   //ホバーしたときにスライドを一時停止しない　
+    });
+});
