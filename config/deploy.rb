b0fae66b3c32879cfabfdeb153a330bfd319c49c
yoiyo <aden.yi21120821@gmail.com>
2021-10-12 17:49:47 +0900

    デプロイエラ―修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,24 @@
 # config valid only for current version of Capistrano
 lock '3.16.0'
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'MamaPapa'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/yoshihiro-ichi/mamapapa.git'
 # deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/mamapapa'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 # 保持するバージョンの個数(※後述)
 set :keep_releases, 5
 # Rubyのバージョン
-set :rbenv_type, :user
+
 set :rbenv_ruby, '2.6.6'
-set :rbenv_type, :debug
-set :rbenv_custom_path, '/root/.rbenv'
+set :rbenv_type, :info
+
 # 出力するログのレベル。エラーログを詳細に見たい場合は :debug に設定する。
 # 本番環境用のものであれば、 :info程度が普通。
 # ただし挙動をしっかり確認したいのであれば :debug に設定する。
